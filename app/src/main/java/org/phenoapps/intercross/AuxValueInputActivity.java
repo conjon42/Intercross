75d0b3d7fa85516daba927ef17c4ec1dedc9efed
chaneylc <chaneylc@gmail.com>
2018-07-23 12:12:30 -0500

    Added async task for barcode printing.

--- app/src/main/java/org/phenoapps/intercross/AuxValueInputActivity.java
+++ app/src/main/java/org/phenoapps/intercross/AuxValueInputActivity.java
@@ -9,6 +9,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -24,6 +25,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zebra.sdk.comm.BluetoothConnection;
@@ -42,6 +44,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+//TODO create separate file for async bluetooth task
+//TODO create bitmap preview of barcode print
 public class AuxValueInputActivity extends AppCompatActivity {
 
     final static private String line_separator = System.getProperty("line.separator");
@@ -129,6 +133,15 @@ public class AuxValueInputActivity extends AppCompatActivity {
         view.setOrientation(LinearLayout.VERTICAL);
         view.addView(recyclerView);
 
+        TextView tv = new TextView(this);
+        tv.setTextSize(24.0f);
+        tv.setText("Cross ID: " + crossId);
+        TextView tv2 = new TextView(this);
+        tv2.setTextSize(24.0f);
+        tv2.setText("Timestamp: " + mTimestamp);
+        view.addView(tv);
+        view.addView(tv2);
+
         Button submitButton = new Button(this);// = entry.editText;
         submitButton.setText("Update");
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -162,64 +175,71 @@ public class AuxValueInputActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.action_print:
-                BluetoothAdapter mBluetoothAdapter = null;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR) {
-                    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-                    Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 
-                    //TODO allow multiple pairs
-                    //TODO wrap in async task
-                    if (pairedDevices.size() == 1) {
-                        BluetoothDevice bd = pairedDevices.toArray(new BluetoothDevice[] {})[0];
-                        Log.d("BT", "PAIRED");
-                        BluetoothConnection bc = new BluetoothConnection(bd.getAddress());
-                        try {
-                            bc.open();
-                            final ZebraPrinter printer = ZebraPrinterFactory.getInstance(bc);
-                            ZebraPrinterLinkOs linkOsPrinter = ZebraPrinterFactory.createLinkOsPrinter(printer);
-                            PrinterStatus printerStatus = (linkOsPrinter != null) ? linkOsPrinter.getCurrentStatus() : printer.getCurrentStatus();
-                            getPrinterStatus(bc);
-                            if (printerStatus.isReadyToPrint) {
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
+                new AsyncTask<Void, Void, Void>() {
 
-                                        Toast.makeText(AuxValueInputActivity.this, "Printer Ready", Toast.LENGTH_LONG).show();
+                    @Override
+                    protected Void doInBackground(Void[] voids) {
+                        BluetoothAdapter mBluetoothAdapter = null;
+                        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 
-                                    }
-                                });
+                        //TODO allow multiple pairs
+                        //TODO wrap in async task
+                        if (pairedDevices.size() == 1) {
+                            BluetoothDevice bd = pairedDevices.toArray(new BluetoothDevice[]{})[0];
+                            Log.d("BT", "PAIRED");
+                            BluetoothConnection bc = new BluetoothConnection(bd.getAddress());
+                            try {
+                                bc.open();
+                                final ZebraPrinter printer = ZebraPrinterFactory.getInstance(bc);
+                                ZebraPrinterLinkOs linkOsPrinter = ZebraPrinterFactory.createLinkOsPrinter(printer);
+                                PrinterStatus printerStatus = (linkOsPrinter != null) ? linkOsPrinter.getCurrentStatus() : printer.getCurrentStatus();
+                                getPrinterStatus(bc);
+                                if (printerStatus.isReadyToPrint) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
 
-                                //printer.sendCommand("! DF RUN.BAT ! UTILITIES JOURNAL SETFF 50 5 PRINT");
-                                //printer.printConfigurationLabel();
-                                //printer.sendCommand("^XA^FO0,0^ADN,36,20^FDCHANEY^FS^XZ");
-                                printer.sendCommand("^XA"
-                                        + "^FWR"
-                                        + "^FO100,75^A0,25,20^FD" + mCrossId + "^FS"
-                                        + "^FO200,75^A0N,25,20"
-                                        + "^BQN,2,10^FDMA" + mCrossId + "^FS"
-                                        + "^FO450,75^A0,25,20^FD" + mTimestamp + "^FS^XZ");
-                                /*printer.printImage(new ZebraImageAndroid(BitmapFactory.decodeResource(getApplicationContext().getResources(),
-                                        R.drawable.intercross_small)), 75,500,-1,-1,false);*/
+                                            Toast.makeText(AuxValueInputActivity.this, "Printer Ready", Toast.LENGTH_LONG).show();
 
-                            } else if (printerStatus.isHeadOpen) {
-                                //helper.showErrorMessage("Error: Head Open \nPlease Close Printer Head to Print");
-                            } else if (printerStatus.isPaused) {
-                                //helper.showErrorMessage("Error: Printer Paused");
-                            } else if (printerStatus.isPaperOut) {
-                                //helper.showErrorMessage("Error: Media Out \nPlease Load Media to Print");
-                            } else {
-                                //helper.showErrorMessage("Error: Please check the Connection of the Printer");
+                                        }
+                                    });
+
+                                    //printer.sendCommand("! DF RUN.BAT ! UTILITIES JOURNAL SETFF 50 5 PRINT");
+                                    //printer.printConfigurationLabel();
+                                    //printer.sendCommand("^XA^FO0,0^ADN,36,20^FDCHANEY^FS^XZ");
+                                    printer.sendCommand("^XA"
+                                            + "^FWR"
+                                            + "^FO100,75^A0,25,20^FD" + mCrossId + "^FS"
+                                            + "^FO200,75^A0N,25,20"
+                                            + "^BQN,2,10^FDMA" + mCrossId + "^FS"
+                                            + "^FO450,75^A0,25,20^FD" + mTimestamp + "^FS^XZ");
+                            /*printer.printImage(new ZebraImageAndroid(BitmapFactory.decodeResource(getApplicationContext().getResources(),
+                                    R.drawable.intercross_small)), 75,500,-1,-1,false);*/
+
+                                } else if (printerStatus.isHeadOpen) {
+                                    //helper.showErrorMessage("Error: Head Open \nPlease Close Printer Head to Print");
+                                } else if (printerStatus.isPaused) {
+                                    //helper.showErrorMessage("Error: Printer Paused");
+                                } else if (printerStatus.isPaperOut) {
+                                    //helper.showErrorMessage("Error: Media Out \nPlease Load Media to Print");
+                                } else {
+                                    //helper.showErrorMessage("Error: Please check the Connection of the Printer");
+                                }
+
+                                bc.close();
+
+                            } catch (ConnectionException e) {
+                                e.printStackTrace();
+                            } catch (ZebraPrinterLanguageUnknownException e) {
+                                e.printStackTrace();
                             }
-
-                            bc.close();
-
-                        } catch (ConnectionException e) {
-                            e.printStackTrace();
-                        } catch (ZebraPrinterLanguageUnknownException e) {
-                            e.printStackTrace();
+                            return null;
                         }
+                        return null;
                     }
-                }
+                }.execute();
                 break;
             default:
                 return super.onOptionsItemSelected(item);
