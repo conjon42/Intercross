6f1e6ab88d66f8a3f67ebcbf9b63de7a72402bdc
chaneylc <chaneylc@gmail.com>
2019-10-11 08:48:51 -0500

    added Android6 support for date export

--- app/src/main/java/org/phenoapps/intercross/util/DateUtil.kt
+++ app/src/main/java/org/phenoapps/intercross/util/DateUtil.kt
@@ -1,13 +1,19 @@
 package org.phenoapps.intercross.util
 
+import android.os.Build
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
+import java.util.*
 
 class DateUtil {
    // fun getTime(): String = LocalDateTime.now().format(
     // /       DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
 
-    fun getTime(): String = LocalDateTime.now().format(DateTimeFormatter.ofPattern(
-            "yyyy-MM-dd_HH:mm:ss.SSS"))
+    fun getTime(): String = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        LocalDateTime.now().format(DateTimeFormatter.ofPattern(
+                "yyyy-MM-dd_HH:mm:ss.SSS"))
+    } else {
+        Calendar.getInstance().time.toString()
+    }
 }
