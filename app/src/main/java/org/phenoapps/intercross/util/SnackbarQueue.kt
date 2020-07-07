7ac8431f2a669c6b61cf63b615bb455a53f4d8f0
chaneylc <chaneylc@gmail.com>
2020-07-07 17:57:32 -0500

    updated snackbar message queue to not redisplay last displayed messages, resets at a fixed period of 5s

--- app/src/main/java/org/phenoapps/intercross/util/SnackbarQueue.kt
+++ app/src/main/java/org/phenoapps/intercross/util/SnackbarQueue.kt
@@ -19,6 +19,8 @@ class SnackbarQueue {
 
     private var mQueue: ArrayList<SnackJob> = ArrayList()
 
+    private var mLastJob: SnackJob? = null
+
     init {
 
         val task = object : TimerTask() {
@@ -26,24 +28,65 @@ class SnackbarQueue {
             override fun run() {
 
                 try {
-                    if (mQueue.isNotEmpty() && !isShowing) {
-                        isShowing = true
-                        val job = mQueue.removeAt(0)
-                        mSnack = Snackbar.make(job.v, job.txt, Snackbar.LENGTH_LONG)
-                        mSnack.setAction(job.actionText) {
-                            job.action()
-                        }
-                        mSnack.show()
 
+                    if (mQueue.isNotEmpty() && !isShowing) {
+
+                        isShowing = true
+
+                        val job = mQueue.removeAt(0)
+
+                        var newJob = true
+
+                        mLastJob?.let {
+
+                            if (it.txt == job.txt) newJob = false
+
+                        }
+
+                        if (newJob) {
+
+                            mSnack = Snackbar.make(job.v, job.txt, Snackbar.LENGTH_LONG)
+
+                            mSnack.setAction(job.actionText) {
+
+                                job.action()
+
+                            }
+
+                            mSnack.show()
+
+                            mLastJob = job
+
+                        } else {
+
+                            isShowing = false
+
+                        }
                     }
+
                 } catch (e : IllegalArgumentException) {
+
                     e.printStackTrace()
+
                 } finally {
+
                     mHandler.postDelayed({ isShowing = false }, 250)
+
+                    //mHandler.postDelayed({ mLastJob = null }, 10000)
                 }
             }
         }
 
+        val reset = object : TimerTask() {
+
+            override fun run() {
+
+                mLastJob = null
+
+            }
+        }
+
+        Timer().scheduleAtFixedRate(reset, 0, 5000)
         Timer().scheduleAtFixedRate(task, 0, 1000)
 
     }
