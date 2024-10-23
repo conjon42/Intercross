59058fc0980b1c74ffcaaa108b8f1d6ed503cd1d
Romil Shah <romil28@somaiya.edu>
2024-10-23 15:09:26 -0400

    About Page updated matching the content of Fieldbook

--- app/src/main/java/org/phenoapps/intercross/fragments/AboutFragment.kt
+++ app/src/main/java/org/phenoapps/intercross/fragments/AboutFragment.kt
@@ -1,12 +1,15 @@
 package org.phenoapps.intercross.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.lifecycleScope
+
 import com.danielstone.materialaboutlibrary.ConvenienceBuilder
 import com.danielstone.materialaboutlibrary.MaterialAboutFragment
 import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem
@@ -14,26 +17,33 @@ import com.danielstone.materialaboutlibrary.items.MaterialAboutItemOnClickAction
 import com.danielstone.materialaboutlibrary.items.MaterialAboutTitleItem
 import com.danielstone.materialaboutlibrary.model.MaterialAboutCard
 import com.danielstone.materialaboutlibrary.model.MaterialAboutList
-import com.michaelflisar.changelog.ChangelogBuilder
-import com.michaelflisar.changelog.classes.ImportanceChangelogSorter
 import com.mikepenz.aboutlibraries.LibsBuilder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import org.phenoapps.intercross.BuildConfig
 import org.phenoapps.intercross.R
-
+import java.net.HttpURLConnection
+import java.net.URL
 
 class AboutFragment : MaterialAboutFragment() {
 
+    private lateinit var updateCheckItem: MaterialAboutActionItem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         requireContext().setTheme(R.style.AppTheme)
-
     }
 
-    public override fun getMaterialAboutList(c: Context): MaterialAboutList {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkForUpdate()
+    }
 
+    override fun getMaterialAboutList(c: Context): MaterialAboutList {
         val appCardBuilder = MaterialAboutCard.Builder()
 
-        // Add items to card
         appCardBuilder.addItem(MaterialAboutTitleItem.Builder()
                 .text(getString(R.string.app_name))
                 .icon(R.mipmap.ic_launcher)
@@ -42,126 +52,176 @@ class AboutFragment : MaterialAboutFragment() {
                 ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_info),
                 getString(R.string.about_version_title),
                 false))
-        appCardBuilder.addItem(MaterialAboutActionItem.Builder()
-                .text(getString(R.string.changelog_title))
+
+        updateCheckItem = MaterialAboutActionItem.Builder()
+                .text(getString(R.string.check_updates_title))
                 .icon(R.drawable.ic_about_changelog)
-                .setOnClickAction { showChangelog(managedShow = false, rateButton = false) }
-                .build())
+                .build()
+        appCardBuilder.addItem(updateCheckItem)
+
+        appCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(c,
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_website),
+                getString(R.string.about_manual_title),
+                false,
+                Uri.parse("https://docs.fieldbook.phenoapps.org/en/latest/intercross.html")))
+
         appCardBuilder.addItem(ConvenienceBuilder.createRateActionItem(c,
                 ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_rate),
                 getString(R.string.about_rate),
                 null
         ))
-        //TODO: Add new translation project
-        appCardBuilder.addItem(MaterialAboutActionItem.Builder()
-                .text(R.string.about_help_translate_title)
-                .icon(R.drawable.ic_about_help_translate)
-                //.setOnClickAction(ConvenienceBuilder.createWebsiteOnClickAction(c, Uri.parse("https://osij6hx.oneskyapp.com/collaboration/project?id=28259")))
-                .build())
+
         val authorCardBuilder = MaterialAboutCard.Builder()
         authorCardBuilder.title(getString(R.string.about_project_lead_title))
         authorCardBuilder.addItem(MaterialAboutActionItem.Builder()
                 .text(getString(R.string.about_developer_trife))
                 .subText(getString(R.string.about_developer_trife_location))
-                .icon(R.drawable.ic_nav_drawer_person)
+                .icon(R.drawable.ic_nv_about)
                 .build())
+
         authorCardBuilder.addItem(ConvenienceBuilder.createEmailItem(c,
                 ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_email),
                 getString(R.string.about_email_title),
                 true,
                 getString(R.string.about_developer_trife_email),
                 "Intercross Question"))
-        authorCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(c,
-                ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_website),
-                "PhenoApps.org",
-                false,
-                Uri.parse("http://phenoapps.org/")))
+
         val contributorsCardBuilder = MaterialAboutCard.Builder()
-        contributorsCardBuilder.title(getString(R.string.about_contributors_title))
+        contributorsCardBuilder.title(getString(R.string.about_support_title))
 
-        //split contribution funding string into two different variables, will be easier to automatically add new app names this way.
-        //can be used like: "$firstContrib ${getString(R.string.app_name)} $afterContrib"
-        val firstContrib = getString(R.string.about_contributors_funding_text_first)
-        val afterContrib = getString(R.string.about_contributors_funding_text_after)
-
-        //TODO: Add contributors link.
         contributorsCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(c,
-                ResourcesCompat.getDrawable(resources, R.drawable.ic_about_contributors, activity?.theme),
-                getString(R.string.about_contributors_developers_title),
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_contributors),
+                getString(R.string.about_contributors_title),
                 false,
-                Uri.parse("https://github.com/PhenoApps/Intercross/graphs/contributors")))
-//        contributorsCardBuilder.addItem(MaterialAboutActionItem.Builder()
-//                .text(getString(R.string.about_translators_title))
-//                .subText(getString(R.string.about_translators_text))
-//                .icon(R.drawable.ic_about_translators)
-//                .build())
-        contributorsCardBuilder.addItem(MaterialAboutActionItem.Builder()
-                .text(getString(R.string.about_contributors_funding_title))
-                .subText("$firstContrib ${getString(R.string.app_name)} $afterContrib")
-                .icon(R.drawable.ic_about_funding)
-                .build())
+                Uri.parse("https://github.com/PhenoApps/Intercross#-contributors")))
+
+        contributorsCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(c,
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_funding),
+                getString(R.string.about_contributors_funding_title),
+                false,
+                Uri.parse("https://github.com/PhenoApps/Intercross#-funding")))
+
         val technicalCardBuilder = MaterialAboutCard.Builder()
         technicalCardBuilder.title(getString(R.string.about_technical_title))
+
         technicalCardBuilder.addItem(MaterialAboutActionItem.Builder()
                 .text(R.string.about_github_title)
                 .icon(R.drawable.ic_about_github)
                 .setOnClickAction(ConvenienceBuilder.createWebsiteOnClickAction(c, Uri.parse("https://github.com/PhenoApps/Intercross")))
                 .build())
+
         technicalCardBuilder.addItem(MaterialAboutActionItem.Builder()
-                .text(R.string.libraries_title)
+                .text(R.string.about_libraries_title)
                 .icon(R.drawable.ic_about_libraries)
                 .setOnClickAction {
                     LibsBuilder()
-                        .withAutoDetect(true)
-                            .withCheckCachedDetection(false)
-                        .withActivityTitle(getString(R.string.libraries_title))
-                        .withLicenseShown(true)
-                        .withVersionShown(true)
-                        .start(requireContext())
+                            .withActivityTitle(getString(R.string.about_libraries_title))
+                            .withAboutIconShown(true)
+                            .withAboutVersionShown(true)
+                            .withAboutAppName(getString(R.string.app_name))
+                            .start(requireContext())
                 }
                 .build())
+
         val otherAppsCardBuilder = MaterialAboutCard.Builder()
         otherAppsCardBuilder.title(getString(R.string.about_title_other_apps))
+
+        otherAppsCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(c,
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_website),
+                "PhenoApps.org",
+                false,
+                Uri.parse("http://phenoapps.org/")))
+
         otherAppsCardBuilder.addItem(MaterialAboutActionItem.Builder()
-                .text("Field-Book")
+                .text("Field Book")
                 .icon(R.drawable.other_ic_field_book)
                 .setOnClickAction(openAppOrStore("com.fieldbook.tracker", c))
                 .build())
+
         otherAppsCardBuilder.addItem(MaterialAboutActionItem.Builder()
                 .text("Coordinate")
                 .icon(R.drawable.other_ic_coordinate)
                 .setOnClickAction(openAppOrStore("org.wheatgenetics.coordinate", c))
                 .build())
-        otherAppsCardBuilder.addItem(MaterialAboutActionItem.Builder()
-                .text("Inventory")
-                .icon(R.drawable.other_ic_inventory)
-                .setOnClickAction(openAppOrStore("org.wheatgenetics.inventory", c))
-                .build())
-        otherAppsCardBuilder.addItem(MaterialAboutActionItem.Builder()
-                .text("Verify")
-                .icon(R.drawable.other_ic_verify)
-                .setOnClickAction(openAppOrStore("org.phenoapps.verify", c))
-                .build())
 
-        return MaterialAboutList(appCardBuilder.build(), authorCardBuilder.build(), contributorsCardBuilder.build(), otherAppsCardBuilder.build(), technicalCardBuilder.build())
+        return MaterialAboutList(
+                appCardBuilder.build(),
+                authorCardBuilder.build(),
+                contributorsCardBuilder.build(),
+                technicalCardBuilder.build(),
+                otherAppsCardBuilder.build()
+        )
     }
 
-    private fun showChangelog(managedShow: Boolean, rateButton: Boolean) {
-        ChangelogBuilder()
-            .withUseBulletList(true) // true if you want to show bullets before each changelog row, false otherwise
-            .withManagedShowOnStart(managedShow) // library will take care to show activity/dialog only if the changelog has new infos and will only show this new infos
-            .withRateButton(rateButton) // enable this to show a "rate app" button in the dialog => clicking it will open the play store; the parent activity or target fragment can also implement IChangelogRateHandler to handle the button click
-            .withSummary(false, true) // enable this to show a summary and a "show more" button, the second paramter describes if releases without summary items should be shown expanded or not
-            .withTitle(getString(R.string.changelog_title)) // provide a custom title if desired, default one is "Changelog <VERSION>"
-            //TODO
-            //.withOkButtonLabel(requireContext().getString(R.string.zxing_button_ok)) // provide a custom ok button text if desired, default one is "OK"
-            .withSorter(ImportanceChangelogSorter())
-                .buildAndShowDialog(activity as AppCompatActivity?, false)
+    private fun checkForUpdate() {
+        val currentVersion = BuildConfig.VERSION_NAME
+        val owner = "PhenoApps"
+        val repo = "Intercross"
+        val apiUrl = "https://api.github.com/repos/$owner/$repo/releases/latest"
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            try {
+                val result = withContext(Dispatchers.IO) {
+                    val url = URL(apiUrl)
+                    val connection = url.openConnection() as HttpURLConnection
+                    connection.requestMethod = "GET"
+
+                    if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                        connection.inputStream.bufferedReader().use { it.readText() }
+                    } else {
+                        null
+                    }
+                }
+
+                result?.let {
+                    val json = JSONObject(it)
+                    val latestVersion = json.getString("tag_name")
+                    val downloadUrl = json.getJSONArray("assets").getJSONObject(0).getString("browser_download_url")
+
+                    val isNewVersionAvailable = isNewerVersion(currentVersion, latestVersion)
+                    showVersionStatus(isNewVersionAvailable, latestVersion, downloadUrl)
+                }
+            } catch (e: Exception) {
+                Log.e("AboutFragment", "Error checking for updates: ${e.message}")
+            }
+        }
     }
 
-//    override fun getActivityTitle(): CharSequence? {
-//        return getString(R.string.mal_title_about)
-//    }
+    private fun showVersionStatus(isNewVersionAvailable: Boolean, latestVersion: String?, downloadUrl: String?) {
+        if (isNewVersionAvailable) {
+            updateCheckItem.text = getString(R.string.found_updates_title)
+            updateCheckItem.subText = latestVersion
+            updateCheckItem.icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_changelog)
+            updateCheckItem.setOnClickAction {
+                downloadUrl?.let {
+                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+                    startActivity(browserIntent)
+                }
+            }
+        } else {
+            updateCheckItem.text = getString(R.string.no_updates_title)
+            updateCheckItem.icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_about_changelog)
+            updateCheckItem.setOnClickAction(null)
+        }
+        refreshMaterialAboutList()
+    }
+
+    private fun isNewerVersion(currentVersion: String, latestVersion: String): Boolean {
+        val currentVersionComponents = currentVersion.split(".")
+        val latestVersionComponents = latestVersion.split(".")
+
+        val versionComponentsToCompare = minOf(currentVersionComponents.size, latestVersionComponents.size)
+
+        for (i in 0 until versionComponentsToCompare) {
+            val currentComponent = currentVersionComponents[i].toInt()
+            val latestComponent = latestVersionComponents[i].toInt()
+
+            if (currentComponent < latestComponent) return true
+            if (currentComponent > latestComponent) return false
+        }
+
+        return false
+    }
 
     private fun openAppOrStore(packageName: String, c: Context): MaterialAboutItemOnClickAction {
         val packageManager = requireContext().packageManager
